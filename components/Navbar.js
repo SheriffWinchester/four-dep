f0d4467c40858b69504734d01af6451102519122
SheriffWinchester <m3llpak@gmail.com>
2022-02-22 14:28:09 +0200

    Work in progress

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import amosovLogo from '/public/amosovlogo1.png'
 
 export default function Navbar(){
     return (
@@ -8,13 +9,11 @@ export default function Navbar(){
                 <li>
                     <Link href='/'>
                         <Image 
-                            src='/public/amosovlogo.png'
-                            alt='Logo of Amosov Institute'
-                            width={500}
-                            height={500}
+                            src={amosovLogo}
+                            width={70}
+                            height={70}
                         />
                     </Link>
-                    
                 </li>
             </ul>
         </nav>
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  
+  return (
+        <Link href='/enter'>
+            <h1>
+                Enter page
+            </h1>
+        </Link>
+  )
 }
