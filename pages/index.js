0030d37ac8d094fdb84fd0b92e9970a6f4e4a8a4
SheriffWinchester <m3llpak@gmail.com>
2022-04-18 15:39:02 +0300

    Working in progress

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 import styles from '../styles/Index.module.css'
+import headerImage from '/public/Header.png'
 
 export default function Index() {
   return (
